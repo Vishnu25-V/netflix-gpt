23d8700fa6106ba3e940ea240edff61920de04a0
Vishnu <vshnvinoj@gmail.com>
2026-04-19 22:26:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Namaste everyone lets build netfix gpt
+    </div>
+  );
+}
+
+export default App;
